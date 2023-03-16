cf69a46588132e8e7e6a15bf799ad307e9270be9
vom <vom@vomui-iMac-2.local>
2023-03-16 12:45:59 +0900

    meet section : 0316

--- JS/userInterface.js
+++ JS/userInterface.js
@@ -1,6 +1,34 @@
-const stickyHaeder = () => {
+ /////////////////////////Header Stiky
+ const header = document.querySelector('#header')
+ const stickyHaeder = () => {
  const scry = window.scrollY;
- console.log(scry);
+ 
+ if(scry > 0){
+  header.classList.add('active');
+ }else{
+  header.classList.remove('active');
+ }
+
+}
+window.addEventListener('scroll', stickyHaeder);
+
+
+
+///////////////////////Mobile Menu Toggle
+const mobileBtn = document.querySelector('.mobile-btn');
+
+
+toggleMobileBtn = (e) => {
+ const target = e.currentTarget; //2.함수가 실행되면 현재 클릭한 지점의 요소를 target변수에 저장
+ console.log(target); //3. target 변수 출력
+ target.classList.toggle('active');
+
+ if(target.classList.contains('active') /*active가 있다면*/){
+  target.classList.remove('not-active') /*not-active를 없애준다*/
+ }else {
+  target.classList.add('not-active'); /*not-active를 추가해준다*/
+ }
 }
 
-window.addEventListener('scroll', stickyHaeder);
+mobileBtn.addEventListener('click', toggleMobileBtn);
+// 1. 클릭했을때 toggleMobileBtn 함수실행
